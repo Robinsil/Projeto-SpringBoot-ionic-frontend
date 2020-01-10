0cb9ed8a42b7771d824b59453ccf68ab7d67f9c3
Silva <robinson.r.silva@accenture.com>
2020-01-09 21:12:00 -0300

    Criando Interceptor para tratamento de Erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -28,8 +28,8 @@ export class CategoriasPage {
     .subscribe(response =>{
         this.items = response;
     },
-      error => {
-        console.log(error);
+      error => { 
+        
       });
     
   }
