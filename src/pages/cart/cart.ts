d2ee288ddbca8fa47ea650217dd4dcd432ea2c0f
Silva <robinson.r.silva@accenture.com>
2020-02-20 16:28:29 -0300

    Terminando as Funcionalidades do carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -4,6 +4,7 @@ import { CartItem } from '../../models/cart-item';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { APICONFIG } from '../../config/api.config';
 import { CartService } from '../../services/domain/cart.service';
+import { ProdutoDTO } from '../../models/produto.dto';
 
 
 
@@ -42,6 +43,27 @@ export class CartPage {
       error =>{});
     }
   }
+
+  removeItem(produto : ProdutoDTO){
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto : ProdutoDTO){
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto : ProdutoDTO){
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+  total(): number{
+    return this.cartService.total();
+  }
+
+  goOn(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
 
 
 }
