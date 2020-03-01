6ec44c135d785bb836756ea72b07943155946038
Silva <robinson.r.silva@accenture.com>
2020-02-29 22:09:32 -0300

    Infinite scroll

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -11,23 +11,20 @@ export class ProdutoService {
   }
 
   findById(produto_id :string){
-
     return this.http.get<ProdutoDTO>(`${APICONFIG.baseUrl}/produtos/${produto_id}`);
   }
 
-  findByCategoria(categoria_id : string) {
-    return this.http.get(`${APICONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+  findByCategoria(categoria_id : string, page : number = 0, linesPages : number = 24) {
+    return this.http.get(`${APICONFIG.baseUrl}/produtos/?categorias=${categoria_id}&page=${page}&linesPages=${linesPages}`);
   }
 
   getSmallImageFromBucket(id : string):Observable<any>{
-
     let url = `${APICONFIG.bucketbaseUrl}/prod${id}-small.jpg`
     return this.http.get(url,{responseType:'blob'});
 
   }
 
   getImageFromBucket(id : string):Observable<any>{
-
     let url = `${APICONFIG.bucketbaseUrl}/prod${id}.jpg`
     return this.http.get(url,{responseType:'blob'});
 
